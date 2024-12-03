46d6322646ca43f4d991fdf2104556377d4e8f9f
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2024-12-03 19:03:08 +0200

    update

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,5 @@
 const API_KEY = '47286351-e716f5558799522fb527bbff6';
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export default class NewGalleryApi {
   constructor() {
